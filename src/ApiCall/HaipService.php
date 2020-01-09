97518c80bbbbc2b7ae510757be0e8cbb29290bd6
Peter Steenbergen <peter@3ws.nl>
2020-01-09 22:52:26 +0800

    HAIP - Update

--- src/ApiCall/HaipService.php
+++ src/ApiCall/HaipService.php
@@ -4,6 +4,15 @@ namespace TransIP\ApiCall;
 
 class HaipService extends AbstractApiCall
 {
+    /**
+     * @var array $loadBalancingMode the kind of load balancing needed.
+     */
+    protected $loadBalancingMode = [
+        'roundrobin',
+        'cookie',
+        'source',
+    ];
+
     public function haip($haipName)
     {
         return $this->adapter->get(sprintf('haips/%s', $haipName));
@@ -12,5 +21,16 @@ class HaipService extends AbstractApiCall
     public function haips()
     {
         return $this->adapter->get('haips');
+    }
+
+    public function order($productName, $description = '')
+    {
+
+        $args = [
+            'productName' => $productName,
+            'description' => $description,
+        ];
+
+        return $this->adapter->post('haips', $args, true);
     }
 }
